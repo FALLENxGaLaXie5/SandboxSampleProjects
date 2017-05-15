fc3fb8be356ebaaafe197a72decf4d17ddcb0c2a
FALLENxGaLaXie5 <steward.joshua@rocketmail.com>
2017-05-15 13:58:52 -0700

    terrain basics

---
+++ ZombieRunnerNewSBX/Assets/ZombieRun.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieRun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown("1"))
+        {
+            GetComponent<Animation>().Play("sprint_left");
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            GetComponent<Animation>().Play("zombie_attack");
+        }
+    }
+}
